c100edfe7968d8f5f89ff6f8276e305567238459
vernsworld <veronica.avola@pcc.edu>
2020-01-14 11:10:54 -0800

    new situation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   // put setup code here
+  createCanvas(640, 480);
 }
 
 function draw() {
   // put drawing code here
+  background(0);
+  ellipse(mouseX, mouseY, 100, 100);
 }
