bf073b4af06594d6f7406f55d0cbdde0664a8664
vernsworld <veronica.avola@pcc.edu>
2020-01-14 11:42:00 -0800

    psychoshit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,22 @@
 function setup() {
   // put setup code here
-  createCanvas(640, 480);
+  createCanvas(1640, 1480);
 }
 
 function draw() {
   // put drawing code here
-  background(0);
-  ellipse(mouseX, mouseY, 100, 100);
+  background(0)
+  push()
+  fill(255 ,80)
+  for (let i=0; i<1000; i++){
+  triangle(i,i,i*2,i*3, i*mouseX, i*mouseY)
+}
+pop()
+push()
+  fill(255, 70)
+  for (let j=0; j<500; j++){
+  rect(j, j, j*3, j*7, j*mouseX, j*mouseY)
+
+  }
+  pop()
 }
