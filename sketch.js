2b6daccc0006d667ef33e376739ec3071984eec8
vernsworld <veronica.avola@pcc.edu>
2020-01-16 12:25:45 -0800

    static

--- sketch.js
+++ sketch.js
@@ -1,17 +1,42 @@
 function setup() {
   // put setup code here
   createCanvas(1400, 900);
+  background(0)
+push() 
+translate(width/2, height/2);
+beginShape();
+fill(0, 60);
+stroke(255, 70);
+for(var i = 0; i < 1000; i++) {
+  var x = random(-1000, 1000);
+  var y = random(-900, 900);
+  vertex(x, y);
+}
+endShape();
+pop()
+
+stroke(0, 50)
+  fill(255, 70)
+  for (let j=0; j<1000; j++){
+  rect(j, j, j, j, j*10, j*2)
+
 }
 
 function draw() {
   // put drawing code here
-  background(0)
-  
+ // background(0)
 
-  fill(255, 70)
-  for (let j=0; j<1000; j++){
-  rect(j, j, j*3, j*7, j*10, j*2)
+ // stroke(0, 50)
+ //  fill(255, 70)
+ //  for (let j=0; j<1000; j++){
+ //  rect(j, j, j, j, j*10, j*2)
 
   }
+
+
+
+
+
+
 
 }
