eb45416022af16416148308ff55fde019d5ff283
vernsworld <veronica.avola@pcc.edu>
2020-01-21 09:48:06 -0800

    why

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   // put setup code here
   createCanvas(1400, 900);
   background(0)
-push() 
+(push)
 translate(width/2, height/2);
 beginShape();
 fill(0, 60);
@@ -14,11 +14,13 @@ for(var i = 0; i < 1000; i++) {
 }
 endShape();
 pop()
+// stroke(0, 50)
+//   fill(255, 70)
+//   for (let j=0; j<1000; j++){
+//   rect(j, j, j, j, j*10, j*2)
 
-stroke(0, 50)
-  fill(255, 70)
-  for (let j=0; j<1000; j++){
-  rect(j, j, j, j, j*10, j*2)
+}
+
 
 }
 
@@ -26,10 +28,11 @@ function draw() {
   // put drawing code here
  // background(0)
 
- // stroke(0, 50)
- //  fill(255, 70)
- //  for (let j=0; j<1000; j++){
- //  rect(j, j, j, j, j*10, j*2)
+ stroke(0, 50)
+  fill(255, 70)
+  for (let j=0; j<1000; j++){
+  rect(j, j, j, j, j*10, j*2)
+
 
   }
 
@@ -39,4 +42,3 @@ function draw() {
 
 
 
-}
