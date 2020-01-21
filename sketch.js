790d6da9b2f2fbf036056611c444d07fe38208c6
vernsworld <veronica.avola@pcc.edu>
2020-01-21 10:54:54 -0800

    gettingclose

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 function setup() {
   // put setup code here
+  frameRate(10);
   createCanvas(1400, 900);
-  background(0);
 
+  background(0);
+}
+
+
+
+function draw() {
+  // put drawing code here
+ // background(0)
+push();
 translate(width/2, height/2);
 beginShape();
 fill(0, 60);
@@ -13,22 +22,20 @@ for(var i = 0; i < 1000; i++) {
   vertex(x, y);
 }
 endShape();
+pop();
 
-}
-
-
-
-function draw() {
-  // put drawing code here
- // background(0)
-
- stroke(0, 50);
-  fill(255, 70);
+stroke(0, 50);
+  fill(255);
   for (let j=0; j<1000; j++){
-  rect(j, j, j, j, j*10, j*2);
-
-quad(mouseX, mouseY, 86, 20, 69, 63, 30, 76);
+    rect(j, j, j, j, j*10, j*2);
   }
+
+  fill(223,255,0, 50);
+  stroke(255,50)
+  quad(mouseX, mouseY, 86, 10, 69, 63, 15, 76);
+    quad(mouseX+40, mouseY+50, 86, 20, 69, 63, 30, 76);
+          quad(mouseX-40, mouseY-50, 86, 20, 69, 63, 30, 76);
+
 
 
 
