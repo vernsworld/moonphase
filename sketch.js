378aea6a82856ad7e7e68a1dc3af96d59eebb581
vernsworld <veronica.avola@pcc.edu>
2020-01-23 11:43:52 -0800

    Revert "first commit"
    This reverts commit b0224b61cc3c5867710acdf1f3eb55c51a33f935.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,44 @@
 function setup() {
   // put setup code here
   //frameRate(25);
- createCanvas(displayWidth, displayHeight);
+  createCanvas(displayWidth, displayHeight);
 
-  background(255, 200, 200);
-  var c = color(255, 255, 0);
-  fill(c);
-  noStroke();
+  background(0);
+//background will be different if you refresh page
+push();
+translate(width/2, height/2);
+beginShape();
+fill(0, 60);
+stroke(255, 70);
+for(var i = 0; i < 1000; i++) {
+  var x = random(-1000, 1000);
+  var y = random(-900, 900);
+  vertex(x, y);
+}
+endShape();
+pop();
 }
 
-    function draw() {
-      // put drawing code here
-      frames++
-    background(100);
-    drawMoon(frames%5, i*10);
-    
+
+
+function draw() {
+  // put drawing code here
+ // background(0)
+//chartreuse triangles
+  fill(223,255,0, 50);
+  stroke(255,50)
+  quad(mouseX, mouseY, 86, 10, 69, 63, 15, 76);
+    quad(mouseX+41, mouseY+50, 86, 20, 69, 63, 30, 76);
+          quad(mouseX-41, mouseY-50, 86, 20, 69, 63, 30, 76);
+
+//psycho rectangle cone
+stroke(0, 50);
+  fill(255);
+  for (let j=0; j<1000; j++){
+    rect(j, j, j, j, j*10, j*2);
+  }
+
+
 }
 
-function drawMoon(moonphase, moonsize){
-fill(244, 244,0);
-ellipse(width/2, height/2, moonsize, moonsize)
-fill(0);
-let ellipseXPosition = map(moonphase, 0, 30, 566, 796); 
-ellipse(mouseX, height/2, moonsize, moonsize)
-print(mouseX);
-//796 - 566 range of X for second ellipse 
-}
+
