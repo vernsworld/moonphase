ae106859eba19ae7c64429fbbca8e9a4e62ec8f9
vernsworld <veronica.avola@pcc.edu>
2020-01-30 10:58:41 -0800

    noecclipse

--- sketch.js
+++ sketch.js
@@ -10,8 +10,10 @@ function draw() {
   frames++
   background(0);
   
-  drawMoon(frames%30, 200);
-
+  drawMoon(200);
+  push()
+  translate(width/6, height/5);
+  drawMoon(100);
 
 }
 
@@ -21,8 +23,5 @@ function drawMoon(moonphase, moonsize){
   //that looks different depeneding on what number is passed to moonphase
   fill(255,255,0);
   ellipse(width/2, height/2, moonsize, moonsize);
-  fill(0);
-  let ellipseXPosition = map(moonphase, 0, 30, 566, 796);
-  ellipse(ellipseXPosition, height/2, moonsize, moonsize);
-  //print(mouseX);
+  
 }
