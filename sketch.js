9c979289612a379088d43720d5e5a78e563b3030
vernsworld <veronica.avola@pcc.edu>
2020-02-05 15:14:52 -0800

    who fucking knows

--- sketch.js
+++ sketch.js
@@ -2,26 +2,30 @@
 let frames = 0;
 
 function setup() {
- createCanvas(windowWidth, windowHeight);
-  
+ createCanvas(windowWidth, windowHeight, WEBGL);
+  //slider = createSlider(200,400,47);
 }
 
 function draw() {
   frames++
-  background(0);
-  
-  drawMoon(200);
-  push()
-  translate(width/6, height/5);
-  drawMoon(100);
-
+    background(50);
+noCursor();
+    drawMoon(35, 50, frames%30, 100);
+     drawMoon(35, 50, frames%15, 200);
+   
 }
 
 
-function drawMoon(moonphase, moonsize){
-  //maybe uses ellipse, or sphere, or some other functions to draw a moon
-  //that looks different depeneding on what number is passed to moonphase
-  fill(255,255,0);
-  ellipse(width/2, height/2, moonsize, moonsize);
+function drawMoon(x, y, phase, location){
+
+      directionalLight(244,50,0,0, 0, -1);
+
+  stroke(5);
+  fill(244);
+  sphere();
+
   
 }
+
+
+
