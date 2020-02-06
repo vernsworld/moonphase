468e68d97ab4ff18760784307a86ccaee0bc1ca3
vernsworld <veronica.avola@pcc.edu>
2020-02-05 17:50:17 -0800

    newmoon

--- sketch.js
+++ sketch.js
@@ -1,31 +1,32 @@
-
-let frames = 0;
-
+let detailY;
 function setup() {
- createCanvas(windowWidth, windowHeight, WEBGL);
-  //slider = createSlider(200,400,47);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  detailY = createSlider(1, 30, 30);
+  detailY.position(10, height + 5);
+  detailY.style('width', '80px');
 }
 
 function draw() {
-  frames++
-    background(50);
-noCursor();
-    drawMoon(35, 50, 40, 100);
-     drawMoon(35, 50, 40, 200);
-   
-}
-
-
-function drawMoon(x, y, phase, location){
-
-      directionalLight(244,50,0,0, 0, -1);
-
-  stroke(5);
-  fill(244);
-  sphere();
-
+  background(0);
+  rotateY(millis() / 500);
+  drawMoon(40, 100, 20, 69);
   
+  push();
+    rotateX(millis() / 2000);
+
+  translate(width/2, height/3);
+  drawMoon();
+  pop();
 }
 
+function drawMoon(x, y, z, w){
+  let dirX = (mouseX / width - 0.5) * 2;
+  let dirY = (mouseY / height - 0.5) * 2;
+  directionalLight(113, 42, 235, -dirX, -dirY, -1);
+    directionalLight(228, 235, 42, dirX, dirY, -0);
+    directionalLight(255, 255, 255, 10, 10, -1);
 
+  fill(188, 222, 53)
+  ellipsoid(x, y, z, w, detailY.value());
 
+}
